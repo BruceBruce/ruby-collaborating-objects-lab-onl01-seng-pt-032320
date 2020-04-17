6de3f9c0d7c07c881b3a5035132073b3bba45b00
bruce willis <fashun_vyctum@yahoo.com>
2020-04-17 21:11:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
     
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
+    artist = file_name.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
